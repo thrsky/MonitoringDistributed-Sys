f3c8e1068773754f5dfb44e98ab9789913e64d00
xujunyu <1098672878@qq.com>
2017-05-14 21:09:45 +0800

    add

--- src/main/java/com/zjut/sys/web/ApiController.java
+++ src/main/java/com/zjut/sys/web/ApiController.java
@@ -1,5 +1,6 @@
 package com.zjut.sys.web;
 
+import com.zjut.sys.dto.CpuDto;
 import com.zjut.sys.pojo.EcsInfo;
 import com.zjut.sys.service.impl.EcsInfoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by thRShy on 2017/5/6.
@@ -23,8 +28,28 @@ public class ApiController {
 
     @GetMapping(value = "/{ip}/info")
     @ResponseBody
-    public EcsInfo getInfo(@PathVariable("ip") String ip, Model model){
-        EcsInfo ecsInfo=ecsInfoService.getEscInfo(ip);
+    public EcsInfo getInfo(@PathVariable("ip") String ip, Model model) {
+        EcsInfo ecsInfo = ecsInfoService.getEscInfo(ip);
         return ecsInfo;
+    }
+
+    /**
+     * @param ip
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "{ip}/{type}/info/{timetype}/{num}")
+    @ResponseBody
+    public List<CpuDto> getData(@PathVariable("ip") String ip,
+                                @PathVariable("type") String type,
+                                @PathVariable("timetype") String timetype,
+                                @PathVariable("num") String num,
+                                Model model) {
+        List<CpuDto> list = new ArrayList<CpuDto>();
+        CpuDto cpuDto = new CpuDto();
+        cpuDto.setTime(new Date());
+        cpuDto.setUsage(0.95);
+        list.add(cpuDto);
+        return list;
     }
 }
