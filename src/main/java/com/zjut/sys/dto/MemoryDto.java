c01dcaeb3ef0d33f5368c453c924b519a7604666
thrsky <1491474343@qq.com>
2017-06-07 09:46:53 +0800

    内存DTO

--- src/main/java/com/zjut/sys/dto/MemoryDto.java
+++ src/main/java/com/zjut/sys/dto/MemoryDto.java
@@ -4,4 +4,43 @@ package com.zjut.sys.dto;
  * Created by xjy on 2017/5/15.
  */
 public class MemoryDto {
+    private double MenToal;
+    private double MenUsed;
+    private double rate;
+
+    public MemoryDto() {
+    }
+
+    public double getMenToal() {
+        return MenToal;
+    }
+
+    public void setMenToal(double menToal) {
+        this.MenToal = menToal;
+    }
+
+    public double getMenUsed() {
+        return MenUsed;
+    }
+
+    public void setMenUsed(double menUsed) {
+        this.MenUsed = menUsed;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "MemoryDto{" +
+                "MenToal=" + MenToal +
+                ", MenUsed=" + MenUsed +
+                ", rate=" + rate +
+                '}';
+    }
 }
