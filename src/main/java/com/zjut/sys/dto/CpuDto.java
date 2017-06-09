09fb358ef9ca7559d01bc64f6cdbd07c55f27248
xujunyu <1098672878@qq.com>
2017-06-10 00:45:17 +0800

    fix cpudto

--- src/main/java/com/zjut/sys/dto/CpuDto.java
+++ src/main/java/com/zjut/sys/dto/CpuDto.java
@@ -1,35 +1,14 @@
 package com.zjut.sys.dto;
 
+import lombok.Data;
+
 import java.util.Date;
 
 /**
  * Created by xjy on 2017/5/14.
  */
+@Data
 public class CpuDto {
     private Date time;
     private double usage;
-
-    public double getUsage() {
-        return usage;
-    }
-
-    public void setUsage(double usage) {
-        this.usage = usage;
-    }
-
-    public Date getTime() {
-        return time;
-    }
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
-
-    @Override
-    public String toString() {
-        return "CpuDto{" +
-                "time=" + time +
-                ", usage=" + usage +
-                '}';
-    }
 }
